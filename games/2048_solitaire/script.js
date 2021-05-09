5a79123e4f0c9588e2f929fb0988649b9656dbfa
Dragon_Fly_02 <vuphanthanglong@gmail.com>
2021-05-09 06:08:55 +0200

    Add files via upload
    Add new game and dragging cards features, working on discard(might differ from the original behavior), the rest requires animationloops so implement tomorrow.

--- games/2048_solitaire/script.js
+++ games/2048_solitaire/script.js
@@ -14,10 +14,177 @@ const cardColor = {
   2048: 'Gold',
 }
 
+let maxCard = 3;
+var discard = document.querySelector('#discard');
+
 main()
 
 function main() {
+  /*
+  Restart button
+  */
+  const restartButton = document.querySelector("#restart_button");
+  restartButton.addEventListener('click', restart);
+  /*
+  Discard
+  */
+  discard.addEventListener('dragenter', discard_dragEnter);
+  discard.addEventListener('dragover', discard_dragOver);
+  discard.addEventListener('dragleave', discard_dragLeave);
+  discard.addEventListener('drop', discard_drop);
 
+  /*
+  Drag and drop cards
+  */
+  const card = document.querySelector("#two");
+  card.addEventListener('dragstart', dragStart);
+  card.addEventListener('dragend', dragEnd);
+  const curs = document.querySelectorAll('.cur');
+  curs.forEach(cur => {
+    cur.addEventListener('dragenter', dragEnter);
+    cur.addEventListener('dragover', dragOver);
+    cur.addEventListener('dragleave', dragLeave);
+    cur.addEventListener('drop', drop);
+  });
+}
+
+function discard_dragEnter(e) {
+  if (discard !== e.target) return;
+  e.preventDefault();
+  e.target.classList.add('drag-over');
+}
+
+function discard_dragOver(e) {
+  if (discard !== e.target) return;
+  e.preventDefault();
+  e.target.classList.add('drag-over');
+}
+
+function discard_dragLeave(e) {
+  if (discard !== e.target) return;
+  e.target.classList.remove('drag-over');
+}
+
+function discard_drop(e) {
+  if (discard !== e.target) return;
+  e.target.classList.remove('drag-over');//forgot what this does but it's important to keep it here
+}
+
+function restart(event) {
+  const columns = document.querySelectorAll(".column");
+  for(const column of columns) {
+    console.log(column.children.length);
+    while (column.children.length > 1)
+      column.removeChild(column.lastChild);
+    column.children[0].classList.add('cur');
+  }
+  let one = document.querySelector("#oneCard");
+  one.textContent = 2;
+  let two = document.querySelector("#twoCard");
+  two.textContent = 2;
+  /*one = document.querySelector("#one");
+  one.style.background = 'grey';
+  two = document.querySelector("#two");
+  two.style.background = 'grey';*/
+  const score = document.querySelector("#score_number");
+  score.textContent = 0;
+  const curs = document.querySelectorAll('.cur');
+  curs.forEach(cur => {
+    cur.addEventListener('dragenter', dragEnter);
+    cur.addEventListener('dragover', dragOver);
+    cur.addEventListener('dragleave', dragLeave);
+    cur.addEventListener('drop', drop);
+  });
+}
+
+function dragStart(e) {
+  console.log('dragStart');
+  setTimeout(() => {
+        e.target.classList.add('hide');
+    });
+}
+
+function dragEnd(e) {
+  const lmao = e.dataTransfer.dropEffect;
+  console.log(lmao);
+  if (lmao == "none")//we keep the same card
+    e.target.classList.remove('hide');
+  else {//we replace with new card
+    const newCard = document.createElement('div');
+    newCard.id = "two";
+    newCard.classList.add('card_tail');
+    newCard.style.left = "100px";
+    newCard.setAttribute("draggable", "true");
+    const newNum = document.createElement('p');
+    newNum.id = "twoCard";
+    newNum.classList.add('card_number');
+    newNum.textContent = 2;
+    newCard.append(newNum);
+    const tail = document.querySelector('#tail');
+    tail.append(newCard);
+    newCard.addEventListener('dragstart', dragStart);
+    newCard.addEventListener('dragend', dragEnd);
+    generateCard(maxCard);
+  }
+}
+
+function dragEnter(e) {
+  console.log('dragOver');
+  e.preventDefault();
+  e.target.classList.add('drag-over');
+}
+
+function dragOver(e) {
+  console.log('dragOver');
+  e.preventDefault();
+  e.target.classList.add('drag-over');
+}
+
+function dragLeave(e) {
+  console.log('dragLeave');
+  e.target.classList.remove('drag-over');
+}
+
+function drop(e) {
+  console.log('drop');
+
+  const card = document.querySelector("#two");//card that we have
+  const len = e.currentTarget.style.top.length;
+  let top = Number(e.currentTarget.style.top.slice(0, len - 2));//we inspect the top attribute
+
+  e.target.classList.remove('cur');
+  card.classList.add('cur');//the new card is now the current card in our column
+
+  const column = document.querySelector(`#c${e.target.getAttribute('value')}`);
+  column.appendChild(card);//add this card to the right column
+
+  /*
+  Initializing the new card in the column.
+  */
+  card.classList.remove('hide');
+  card.classList.remove('card_tail');
+  card.classList.add('card');
+  card.children[0].removeAttribute('id');
+  card.removeAttribute('id');
+  card.removeAttribute("draggable");
+  if (e.currentTarget.getAttribute("top") == "1")
+    top = -30;
+  card.style.top = `${top + 30}px`;
+  card.top = 0;
+  card.style.left = "";
+  card.setAttribute("value", e.target.getAttribute('value'));
+
+  e.target.classList.remove('drag-over');//forgot what this does but it's important to keep it here
+  card.addEventListener('dragenter', dragEnter);
+  card.addEventListener('dragover', dragOver);
+  card.addEventListener('dragleave', dragLeave);
+  card.addEventListener('drop', drop);
+
+  //remove all the listener from the previous card
+  e.target.removeEventListener('dragenter', dragEnter);
+  e.target.removeEventListener('dragover', dragOver);
+  e.target.removeEventListener('dragleave', dragLeave);
+  e.target.removeEventListener('drop', drop);
 }
 
 function generateCard(maxCard) {
